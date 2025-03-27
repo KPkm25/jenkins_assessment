870e733285e40478c47d845dc0c86117494c44b9
KPkm25 <kumarparakram007@gmail.com>
2025-03-27 17:19:55 +0530

    tests

--- pytest/test.py
+++ pytest/test.py
@@ -1,2 +1,9 @@
-def check():
-	assert 1+1==2
+import unittest
+
+class Check(unittest.TestCase):
+    def test_math(self):
+        self.assertEqual(1 + 1, 2)
+        self.assertEqual(1, 1)
+
+if __name__ == "__main__":
+    unittest.main()
